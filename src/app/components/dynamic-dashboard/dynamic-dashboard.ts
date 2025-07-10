0d0b869bcc242d4c9e5cd892c1353692906c8d8b
Ahmed8881 <buttahmed2004@gmail.com>
2025-07-09 22:49:49 -0700

    Add DynamicDashboard component with HTML template, SCSS styles, and unit tests

---
+++ src/app/components/dynamic-dashboard/dynamic-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-dashboard',
+  imports: [],
+  templateUrl: './dynamic-dashboard.html',
+  styleUrl: './dynamic-dashboard.scss'
+})
+export class DynamicDashboard {
+
+}
